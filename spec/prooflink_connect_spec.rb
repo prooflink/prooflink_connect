54a02584a547198bbbc1b430aa6ea95ae7d9776e
Chiel Wester <chiel.wester@holder.nl>
2011-01-17 16:15:40 +0100

    Add rspec

---
+++ spec/prooflink_connect_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe ProoflinkConnect do
+  it "should work" do
+    pending "tests are to come"
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+require 'rubygems'
+require 'rspec'
+require 'rspec/autorun'
+# require 'rack/test'
+# require 'webmock/rspec'
+
+# include Rack::Test::Methods
+# include WebMock
+
+require 'prooflink_connect'
+
+# WebMock.disable_net_connect!
