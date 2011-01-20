132b125f3db31857ccfa77c24e6d96e03c4d904b
Chiel Wester <chiel.wester@holder.nl>
2011-01-20 15:07:13 +0100

    Add forced connect parameter to embedded iframe to force the connect screen to show

--- lib/prooflink_connect.rb
+++ lib/prooflink_connect.rb
@@ -13,6 +13,11 @@ module ProoflinkConnect
 
   def self.embedded(options = {})
     options = {:subdomain => ProoflinkConnect.config.subdomain, :token_url => 'https://example.com/auth/callbacks'}.merge(options)
-    "<iframe src='#{ProoflinkConnect.config.protocol}://#{[options[:subdomain], ProoflinkConnect.config.provider_endpoint].compact.join(".")}/authentications/embedded?token_url=#{options[:token_url]}' style='width: 500px; height: 220px; border: 0;display: block' ></iframe>".html_safe
+
+    if options[:forced_connect]
+      "<iframe src='#{ProoflinkConnect.config.protocol}://#{[options[:subdomain], ProoflinkConnect.config.provider_endpoint].compact.join(".")}/authentications/embedded?token_url=#{options[:token_url]}&forced_connect=1' style='width: 500px; height: 220px; border: 0;display: block' ></iframe>".html_safe
+    else
+      "<iframe src='#{ProoflinkConnect.config.protocol}://#{[options[:subdomain], ProoflinkConnect.config.provider_endpoint].compact.join(".")}/authentications/embedded?token_url=#{options[:token_url]}' style='width: 500px; height: 220px; border: 0;display: block' ></iframe>".html_safe
+    end
   end
 end
