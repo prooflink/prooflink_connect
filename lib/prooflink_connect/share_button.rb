5f1ad7e9f4b5574b18da8a1600754ff9d3bdf59a
Chiel Wester <chiel.wester@holder.nl>
2011-05-27 16:22:45 +0200

    Add options to share buttons

--- lib/prooflink_connect/share_button.rb
+++ lib/prooflink_connect/share_button.rb
@@ -1,13 +1,15 @@
 module ProoflinkConnect
   class ShareButton
     attr_accessor :message
+    attr_accessor :options
 
-    def initialize(message = nil)
+    def initialize(message = nil, options = {})
       @message = message
+      @options = options
     end
 
     def to_html
-      "<iframe src='#{ProoflinkConnect.config.protocol}://#{[ProoflinkConnect.config.subdomain, ProoflinkConnect.config.provider_endpoint].compact.join(".")}/shares/button?message=#{CGI.escape @message}' style='width: 100px; height: 100px; border: 0;display: block' frameborder='0'></iframe>".html_safe
+      "<iframe src='#{ProoflinkConnect.config.protocol}://#{[ProoflinkConnect.config.subdomain, ProoflinkConnect.config.provider_endpoint].compact.join(".")}/#{@options[:locale]||"en"}/shares/button?message=#{CGI.escape @message}' style='width: 100px; height: 100px; border: 0;display: block' frameborder='0'></iframe>".html_safe
     end
   end
 end
