409604d3e841ede5709ecd3f74d085f4629d2971
Mark Mulder <markmulder@gmail.com>
2011-08-17 15:57:19 +0800

    Test base_uri method on configuration.

---
+++ spec/prooflink_connect/configuration_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ProoflinkConnect::Configuration do
+  it "provides defaults" do
+    ProoflinkConnect.config.base_uri.should == "https://example.prooflink.com"
+  end
+
+  it "returns correct url" do
+    ProoflinkConnect.configure do |config|
+      config.provider_endpoint = "local-prooflink.com"
+      config.subdomain = "testing"
+      config.protocol = "http"
+    end
+    ProoflinkConnect.config.base_uri.should == "http://testing.local-prooflink.com"
+  end
+end
+
