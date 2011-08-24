543a1feb85fe396d58c5401e071aaad3d4be24ac
Mark Mulder <markmulder@gmail.com>
2011-08-24 13:52:58 +0200

    Add support for position param in iframe url
    This will be used to track from which location 
    the message has been posted.

--- spec/prooflink_connect_spec.rb
+++ spec/prooflink_connect_spec.rb
@@ -25,4 +25,16 @@ describe ProoflinkConnect do
     result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0' style='width: 520px; height: 100px; border: 0; display: block' frameborder='0' allowTransparency='true'></iframe>"
     ProoflinkConnect.embedded({:height => "100"}).should == result
   end
+
+  context "position" do
+    it "query param" do
+      result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&position=spec' style='width: 520px; height: 250px; border: 0; display: block' frameborder='0' allowTransparency='true'></iframe>"
+      ProoflinkConnect.embedded(:position => "spec").should == result
+    end
+
+    it "url encodes input" do
+      result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&position=spec+with+spaces' style='width: 520px; height: 250px; border: 0; display: block' frameborder='0' allowTransparency='true'></iframe>"
+      ProoflinkConnect.embedded(:position => "spec with spaces").should == result
+    end
+  end
 end
