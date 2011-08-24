cde82a756c2176c36e58a74bb7680e88a71ea76b
Mark Mulder <markmulder@gmail.com>
2011-08-24 14:42:43 +0200

    Update spec to work in 1.9.2 as well.
    1.9.2 has ordered hashes by default so we need to 
    use a custom matcher for webmock as webmock cares 
    in what order the params are.
    
    https://github.com/bblimke/webmock/issues/69

--- spec/prooflink_connect/share_spec.rb
+++ spec/prooflink_connect/share_spec.rb
@@ -6,9 +6,10 @@ describe ProoflinkConnect::Share do
   end
 
   it "posts a message to Prooflink" do
+    expected_parameters = {"format"=>["json"], "api_key"=>["1234"], "message"=>["This is pretty awesome"], "transaction"=>["pl2"]}
     stub_request(:post, "https://example.prooflink.com/shares/post_share_transaction").
-      with(:body => "format=json&api_key=1234&message=This%20is%20pretty%20awesome&transaction=pl1").
+      with{|request| expected_parameters == CGI.parse(request.body)}.
       to_return(:status => 200, :body => "", :headers => {})
-    ProoflinkConnect::Share.post("pl1", "This is pretty awesome")
+    ProoflinkConnect::Share.post("pl2", "This is pretty awesome")
   end
 end
