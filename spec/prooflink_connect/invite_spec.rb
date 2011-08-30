039bb3fa670951fb903d6447ce4e33af001b81c9
Mark Mulder <markmulder@gmail.com>
2011-08-30 12:03:58 +0200

    Use custom webmock matcher for invite_spec

--- spec/prooflink_connect/invite_spec.rb
+++ spec/prooflink_connect/invite_spec.rb
@@ -11,13 +11,13 @@ describe ProoflinkConnect::Invite do
     stub_request(:post, "https://example.prooflink.com/invites").
       with(:body => "invite[email]=newuser%40prooflink.com&api_key=1234&locale=nl").
       to_return(:status => 200, :body => {
-      "entry" => {
-      "name" => {},
-      "displayName" => "Prooflink",
-      "id" => "cg6a1Turx70NWzkkwrbGRDRvImY=\n",
-      "invite_url" => "url"
-    }
-    }.to_json, :headers => {})
+        "entry" => {
+          "name" => {},
+          "displayName" => "Prooflink",
+          "id" => "cg6a1Turx70NWzkkwrbGRDRvImY=\n",
+          "invite_url" => "url"
+        }
+      }.to_json, :headers => {})
 
     invite = ProoflinkConnect::Invite.new(attributes)
     invite.save.should == true
@@ -26,20 +26,21 @@ describe ProoflinkConnect::Invite do
 
   it "creates a Prooflink invite with custom parameters" do
     attributes = {:email => 'newuser@prooflink.com'}
+    expected_parameters = {"locale"=>["nl"], "api_key"=>["4567"], "invite[email]"=>["newuser@prooflink.com"]}
 
     stub_request(:post, "https://example.prooflink.com/invites").
-      with(:body => "api_key[]=4567&invite[email]=newuser%40prooflink.com&locale=nl").
+      with{|request| expected_parameters == CGI.parse(request.body)}.
       to_return(:status => 200, :body => {
-      "entry" => {
-      "name" => {},
-      "displayName" => "Prooflink",
-      "id" => "cg6a1Turx70NWzkkwrbGRDRvImY=\n",
-      "invite_url" => "url"
-    }
-    }.to_json, :headers => {})
+        "entry" => {
+          "name" => {},
+          "displayName" => "Prooflink",
+          "id" => "cg6a1Turx70NWzkkwrbGRDRvImY=\n",
+          "invite_url" => "url"
+        }
+      }.to_json, :headers => {})
 
     invite = ProoflinkConnect::Invite.new(attributes)
-    invite.save({"api_key" => ["4567"]}).should == true
+    invite.save({"api_key" => "4567"}).should == true
   end
 
 end
