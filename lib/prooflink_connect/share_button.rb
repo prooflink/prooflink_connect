bc6c606bf3b053a470d8650fbb58082e9d5c002a
Chiel Wester <chiel.wester@holder.nl>
2011-11-11 14:03:15 +0100

    Fix the size of the share button

--- lib/prooflink_connect/share_button.rb
+++ lib/prooflink_connect/share_button.rb
@@ -9,7 +9,7 @@ module ProoflinkConnect
     end
 
     def to_html
-      html = "<iframe src='#{ProoflinkConnect.config.protocol}://#{[ProoflinkConnect.config.subdomain, ProoflinkConnect.config.provider_endpoint].compact.join(".")}/#{@options[:locale]||"en"}/shares/button?message=#{CGI.escape @message}' style='width: 102px; height: 100px; border: 0;display: block' frameborder='0'></iframe>"
+      html = "<iframe src='#{ProoflinkConnect.config.protocol}://#{[ProoflinkConnect.config.subdomain, ProoflinkConnect.config.provider_endpoint].compact.join(".")}/#{@options[:locale]||"en"}/shares/button?message=#{CGI.escape @message}' style='width: 112px; height: 16px; border: 0;display: block' frameborder='0' scrolling='no'></iframe>"
       html.respond_to?(:html_safe) ? html.html_safe : html
     end
   end
