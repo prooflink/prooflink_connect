5d1c6808e006f1dd386f4e180b8dd292dd8f9ffb
Mark Mulder <markmulder@gmail.com>
2011-12-21 14:49:53 +0100

    Add use_popups setting
    embed_forms will be deprecated, use_popups will need to be
    set to false to have the same effect as embed_forms true.

--- lib/prooflink_connect.rb
+++ lib/prooflink_connect.rb
@@ -28,14 +28,19 @@ module ProoflinkConnect
       :subdomain => config.subdomain,
       :token_url => 'https://example.com/auth/callbacks',
       :forced_connect => false,
-      :embed_forms => false,
+      :embed_forms => false, # will be deprecated
+      :use_popups => true,
       :split_screen => false,
       :width => '520px',
       :height => '250px'}.merge(options)
 
     domain_part = [options[:subdomain], config.provider_endpoint].compact.join(".")
     path_part = [options[:locale], 'authentications', 'embedded'].compact.join("/")
-    query_part = "token_url=#{options[:token_url]}&forced_connect=#{options[:forced_connect] ? 1 : 0}&embed_forms=#{options[:embed_forms] ? 1 : 0}&split_screen=#{options[:split_screen] ? 1 : 0}"
+    query_part = "token_url=#{options[:token_url]}"
+    query_part << "&forced_connect=#{options[:forced_connect] ? 1 : 0}"
+    query_part << "&embed_forms=#{options[:embed_forms] ? 1 : 0}"
+    query_part << "&split_screen=#{options[:split_screen] ? 1 : 0}"
+    query_part << "&use_popups=#{options[:use_popups] ? 1 : 0}"
     frame_url = "#{config.protocol}://#{domain_part}/#{path_part}?#{query_part}"
     html = "<iframe src='#{frame_url}' style='width: #{options[:width]}; height: #{options[:height]}; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
     html.respond_to?(:html_safe) ? html.html_safe : html
--- spec/prooflink_connect_spec.rb
+++ spec/prooflink_connect_spec.rb
@@ -2,37 +2,44 @@ require 'spec_helper'
 
 describe ProoflinkConnect do
   it "returns iframe html with defaults" do
-    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0' style='width: 520px; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
+    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0&use_popups=1' style='width: 520px; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
     ProoflinkConnect.embedded.should == result
   end
 
   it "allows pixels or percentage for width" do
-    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0' style='width: 100%; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
+    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0&use_popups=1' style='width: 100%; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
     ProoflinkConnect.embedded({:width => "100%"}).should == result
   end
 
   it "adds px to width if not given" do
-    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0' style='width: 100px; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
+    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0&use_popups=1' style='width: 100px; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
     ProoflinkConnect.embedded({:width => "100"}).should == result
   end
 
   it "allows pixels or percentage for height" do
-    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0' style='width: 520px; height: 100%; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
+    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0&use_popups=1' style='width: 520px; height: 100%; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
     ProoflinkConnect.embedded({:height => "100%"}).should == result
   end
 
   it "adds px to height if not given" do
-    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0' style='width: 520px; height: 100px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
+    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0&use_popups=1' style='width: 520px; height: 100px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
     ProoflinkConnect.embedded({:height => "100"}).should == result
   end
 
-  it "accept boolean options as boolean" do
-    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=1&split_screen=1' style='width: 520px; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
-    ProoflinkConnect.embedded(:split_screen => true, :embed_forms => true).should == result
+  context "split screen" do
+    it "accept boolean options as boolean" do
+      result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=1&split_screen=1&use_popups=1' style='width: 520px; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
+      ProoflinkConnect.embedded(:split_screen => true, :embed_forms => true).should == result
+    end
+
+    it "accept boolean options as strings" do
+      result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=1&split_screen=1&use_popups=1' style='width: 520px; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
+      ProoflinkConnect.embedded(:split_screen => '1', :embed_forms => '1').should == result
+    end
   end
 
-  it "accept boolean options as strings" do
-    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=1&split_screen=1' style='width: 520px; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
-    ProoflinkConnect.embedded(:split_screen => '1', :embed_forms => '1').should == result
+  it "use_popups" do
+    result = "<iframe src='https://example.prooflink.com/authentications/embedded?token_url=https://example.com/auth/callbacks&forced_connect=0&embed_forms=0&split_screen=0&use_popups=1' style='width: 520px; height: 250px; border: 0; display: block;' frameborder='0' allowTransparency='true'></iframe>"
+    ProoflinkConnect.embedded(:use_popups => true).should == result
   end
 end
